665e471b36ae97043cef3627f974e2cbde68ef2f
GennadiPyton <73903012+GennadiPyton@users.noreply.github.com>
2021-12-10 18:56:30 +0200

    RegisterUser

--- controller/Controller.php
+++ controller/Controller.php
@@ -53,4 +53,16 @@ class Controller {
         ViewComments::CommentsCountWithAncor($arr);
     }
 
-}
+    // ------------------------РЕГИСТРАЦИЯ
+    public function registerForm()
+    {
+        include_once('view/formRegister.php');
+    }
+    public function registerUser()
+    {
+        $result = Register::registerUser();
+
+        include_once('view/answerRegister.php');
+    }
+
+}//class
